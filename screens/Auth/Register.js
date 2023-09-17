fd7b83f5fc7bf9b5abb29688c813b691ed4df3f6
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-09-18 01:20:49 +0700

    Mengganti dengan formik

--- screens/Auth/Register.js
+++ screens/Auth/Register.js
@@ -1,52 +1,84 @@
-import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
 import { SafeAreaView } from 'react-native-safe-area-context'
 import auth from '../../config/FIRESTORE'
+import * as yup from 'yup'
+import { Formik } from 'formik'
+
+const Register = ({ navigation }) => {
+    const [formValue, setFormValues] = useState({
+        email: "",
+        password: ""
+    })
 
 
-const Register = ({navigation}) => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [message, setMessage] = useState("")
+    const validationSchema = yup.object().shape({
+        email: yup.string().email("Email tidak valid").required("Email wajib diisi"),
+        password: yup.string().min(6, "Password minimal harus 6 karakter").required("Password wajib diisi")
+    })
 
-    const handleRegister = async () => {
+    const handleRegister = async (formValues) => {
         try {
-            const auth = getAuth()
-            const isUserCreated = await createUserWithEmailAndPassword(auth, email, password)
-            navigation.navigate("Login");
-
-            setMessage(Alert.alert("Success", "User registered successfully."));
-
-            setPassword("");
-            setEmail("");
+            await validationSchema.validate(formValues, { abortEarly: false })
+            const authInstance = getAuth()
+            await createUserWithEmailAndPassword(authInstance, formValues.email, formValues.password)
+            navigation.navigate('Login')
         } catch (error) {
-            setMessage(Alert.alert("Failed", isUserCreated.message))
+            console.log('Error during registration:', error)
         }
     }
+
 
     return (
         <SafeAreaView>
             <View className="container px-4 flex h-full w-full justify-center">
                 <Text className="my-2 font-bold text-[20px] text-slate-700 text-center">Metahub</Text>
-                <TextInput
-                    className="bg-white border border-slate-700 p-3 rounded text-slate-600 mb-2"
-                    placeholder='Enter Your Email'
-                    value={email}
-                    onChangeText={(value) => setEmail(value)}
-                />
-                <TextInput
-                    className="bg-white border border-slate-700 p-3 rounded text-slate-600 mb-2"
-                    placeholder='Enter Your Password'
-                    value={password}
-                    onChangeText={(value) => setPassword(value)}
-                    secureTextEntry={true}
-                />
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={validationSchema}
+                    onSubmit={(values, { resetForm }) => {
+                        handleRegister(values)
+                        resetForm()
+                    }}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                        <View>
+                            <TextInput
+                                name="email"
+                                className="bg-white border border-slate-700 p-3 rounded text-slate-600"
+                                placeholder="Enter Your Email"
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                            />
+                            {touched.email && errors.email && (
+                                <Text className="text-red-500 mb-2 mt-0 text-[12px]">{errors.email}</Text>
+                            )}
 
+                            <TextInput
+                                name="password"
+                                className="bg-white border border-slate-700 p-3 rounded text-slate-600 "
+                                placeholder="Enter Your Password"
+                                secureTextEntry
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                            />
+                            {touched.password && errors.password && (
+                                <Text className="text-red-500 mb-2 mt-0 text-[12px]">{errors.password}</Text>
+                            )}
 
-                <TouchableOpacity className="bg-sky-700 rounded p-4 mt-2" onPress={() => handleRegister()}>
-                    <Text className="text-white text-center font-bold">Register</Text>
-                </TouchableOpacity>
+                            <TouchableOpacity
+                                className="bg-sky-700 rounded p-4 mt-2"
+                                onPress={handleSubmit}
+                            >
+                                <Text className="text-white text-center font-bold">Register</Text>
+                            </TouchableOpacity>
+
+                        </View>
+                    )}
+                </Formik>
 
                 <View className="flex flex-nowrap flex-row justify-center my-2">
                     <Text>Already have an account? </Text>
